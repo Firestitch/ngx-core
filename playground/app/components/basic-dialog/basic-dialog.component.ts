7dda70441fc932e4840f1f878dcd930da5b55cae
Eugene Kubesh <codbasters@gmail.com>
2021-05-06 14:51:13 +0300

    upgrade to ng 9

--- playground/app/components/basic-dialog/basic-dialog.component.ts
+++ playground/app/components/basic-dialog/basic-dialog.component.ts
@@ -1,16 +1,19 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'basic-dialog',
-  templateUrl: 'basic-dialog.component.html'
+  templateUrl: './basic-dialog.component.html'
 })
 export class BasicDialogComponent {
 
-  public object = {};
+  public object: any = {};
+  public input = '';
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-              private dialogRef: MatDialogRef<BasicDialogComponent>) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<BasicDialogComponent>
+  ) {}
 
   save() {
     this.dialogRef.close();
