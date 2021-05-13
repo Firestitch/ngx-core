95d7655f64903c8d9ac1582bae8ad902ba27d426
Eugene Kubesh <codbasters@gmail.com>
2021-05-13 12:51:11 +0300

    add control container factory

---
+++ src/app/helpers/control-container-factory.ts
@@ -0,0 +1,6 @@
+import { NgForm } from '@angular/forms';
+
+
+export function controlContainerFactory(ngForm?: NgForm) {
+  return ngForm;
+}
--- src/public_api.ts
+++ src/public_api.ts
@@ -9,3 +9,4 @@ export { FsCoreModule } from './app/fs-core.module';
 // Classes
 export { RouteObserver } from './app/classes/route-observer';
 export { RouteSubject } from './app/classes/route-subject';
+export { controlContainerFactory } from './app/helpers/control-container-factory';
